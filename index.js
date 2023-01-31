2d96ebc0596e4f4bbb494065ab146c9888058c41
jbonilla45 <jbonilla45@hotmail.com>
2023-01-31 11:44:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const cors = require("cors");
 
 const app = express();
 
-// app.listen(process.env.PORT, () => console.log('Server on port:'+process.env.PORT));
+app.listen(process.env.PORT, () => console.log('Server on port:'+process.env.PORT));
 
-app.listen(4000, () => {
-    console.log("El servidor esta corriendo");
-  });
+// app.listen(4000, () => {
+//     console.log("El servidor esta corriendo");
+//   });
 
 app.use(express.json());
 app.use(cors());
